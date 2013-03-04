1df66b010e5202a65703fb95c055c6bc6b4fa520
Jeff Sisson <jeffsisson@gmail.com>
2013-03-03 19:34:39 -0500

    some encoding quirks

--- script.js
+++ script.js
@@ -179,13 +179,12 @@ function load() {
 			      .attr("xmlns", "http://www.w3.org/2000/svg")
 			      .node().parentNode.innerHTML;
 			 html = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" + html;
-			  html = html.replace('</defs>', "<style type='text/css'><![CDATA["+response.responseText+"]]></style></defs>")
-			  html = html.replace("<svg class=\"svg\"","<svg class=\"svg "+document.body.className+"\"");
+			  html = html.replace('<defs>', "<defs><style type='text/css'><![CDATA["+response.responseText+"]]></style>")
+			  html = html.replace("class=\"svg\"","class=\"svg "+document.body.className+"\"");
 			  d3.select("body").select("#download")
 			      .attr("title", "file.svg")
 			      .attr("href-lang", "image/svg+xml")
 			      .attr("href", "data:image/svg+xml;base64,\n" + btoa(unescape(encodeURIComponent( html ))))
-			      
 
 		});
 
