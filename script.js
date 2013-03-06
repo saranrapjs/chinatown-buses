0017023eda289e0eb5bd1e02ecbba7caf21f182b
Jeff Sisson <jeffsisson@gmail.com>
2013-03-06 08:30:46 -0500

    bookmarkability

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var foldChange,
-	belowTheFold = false;
+	belowTheFold = false,
+	hashChecked = false;
 function scroll(direction) {
 	function checkScroll() {
 		return (direction == true)
@@ -88,91 +89,121 @@ function load() {
 			// .attr("y",0)
 
 	d3.json("cities.json", function(error, json) { //http://www.census.gov/statab/ccdb/cit1020r.txt
-		var cities = json.cities;
+		var cities = json.cities,
+			originalHash = (window.location.hash.length) ? window.location.hash.substring(1).split("+") : null
+
 		// population filter:
 		// cities = cities.filter(function(city) {
 		// 	return (city.population > 300000)
 		// })
 		var places = d3.shuffle(cities).slice(0,Math.floor(Math.random() * 6) + 2),
+			hashers = [],
 			root = places.pop();
 		root.children = places;
+
+		if (originalHash && originalHash.length > 1 && hashChecked === false) { // we're retrieving a bookmark
+			hashChecked = true;
+			var root = {};
+			originalHash.forEach(function(pop,i) {
+				pop = parseInt(pop)
+				cities.some(function(c) {
+					if (c.population === pop) {
+						if (i===0) {
+							root = c;
+							root.children = [];
+						} else {
+							root.children.push(c)
+						}
+						return true;
+					}
+				})
+			})
+		}
+
 		if (root.children.length === 1) root.children[0].single = true;
 
-			var nodes = cluster.nodes(root),
-				links = cluster.links(nodes);
+		hashers.push(root.population)
+		root.children.forEach(function(c) {
+			hashers.push(c.population)
+		});
+		window.location.hash = hashers.join("+")
 
-			var link = svg.selectAll(".link")
-					.data(links)
-					.enter()
-					.append("path")
-					.attr("marker-end", function(d) { return "url(#End)"; })
-					.attr("marker-start", function(d) { return (root.children.length === 1) ? "url(#Start)" : null; })
-					.attr("class", "link line")
-					.attr("d", function(d,i){
-						var total = links.length-1,
-							shift = (i/total)-.5,
-							shifted = d.source.x;
-						if (!isNaN(shift)) shifted = d.source.x + (shift)*50;
-						return diagonal([
-							[d.source.y,shifted],
-							[d.target.y,d.target.x]
-						]);
-					});
 
-			var node = svg.selectAll(".node")
-					.data(nodes)
-				.enter().append("g")
-					.attr("class", "node")
-					.attr("transform", function(d) { 
-						return "translate(" + d.y + "," + d.x + ")"; 
-					})
+		var nodes = cluster.nodes(root),
+			links = cluster.links(nodes);
 
-			node.append("text")
-					.attr("dx", function(d) { return d.children ? -8 : 8; })
-					.attr("dy", 3)
-					.style("text-anchor", function(d) { return d.children ? "end" : "start"; })
-					.text(function(d) {
-						var showState = !! Math.round(Math.random() * 1),
-							name = d.name.toUpperCase();
-						name = (showState) ? name : name.replace(/\,\s[A-Z]{2,}/i,"");
-						if (d.mandarin && showState === false) name = name + " " + d.mandarin + "";
+		var link = svg.selectAll(".link")
+				.data(links)
+				.enter()
+				.append("path")
+				.attr("marker-end", function(d) { return "url(#End)"; })
+				.attr("marker-start", function(d) { return (root.children.length === 1) ? "url(#Start)" : null; })
+				.attr("class", "link line")
+				.attr("d", function(d,i){
+					var total = links.length-1,
+						shift = (i/total)-.5,
+						shifted = d.source.x;
+					if (!isNaN(shift)) shifted = d.source.x + (shift)*50;
+					return diagonal([
+						[d.source.y,shifted],
+						[d.target.y,d.target.x]
+					]);
+				});
 
-						return name; 
-					})
-					.attr("transform", function(d) { // slightly nasty
-						var y = this.getBBox().y,
-							height = this.getBBox().height,
-							off = y + (height/2) - 20
-						return "scale(1,2) translate(0,"+((-1*off)*1)/2+")"
-					})
-					.attr('class',function(d) {
-						return (d.depth === 0 || d.single && d.single === true)
-							? 'source'
-							: 'destination'
-					})
-					.each(function(d,i){
-						var me = d3.select(this);
-						if (d.depth === 0 || d.single && d.single === true) {
+		var node = svg.selectAll(".node")
+				.data(nodes)
+			.enter().append("g")
+				.attr("class", "node")
+				.attr("transform", function(d) { 
+					return "translate(" + d.y + "," + d.x + ")"; 
+				})
 
-							d3.select(this.parentNode)
-								.attr("transform", function(d) {
-									d.x = d.x + 20;  // accounting for size
-									return "translate(" + d.y + "," + d.x + ")";
-								})
-							if (d.depth === 0) {
-								var offset = this.getBBox().width;
-								svg.attr("transform", "translate("+(offset+10)+",0)")
-							}
+		node.append("text")
+				.attr("dx", function(d) { return d.children ? -8 : 8; })
+				.attr("dy", 3)
+				.style("text-anchor", function(d) { return d.children ? "end" : "start"; })
+				.text(function(d) {
+					var showState = !! Math.round(Math.random() * 1),
+						name = d.name.toUpperCase();
+					name = (showState) ? name : name.replace(/\,\s[A-Z]{2,}/i,"");
+					if (d.mandarin && showState === false) name = name + " " + d.mandarin + "";
+
+					return name; 
+				})
+				.attr("transform", function(d) { // slightly nasty
+					var y = this.getBBox().y,
+						height = this.getBBox().height,
+						off = y + (height/2) - 20
+					return "scale(1,2) translate(0,"+((-1*off)*1)/2+")"
+				})
+				.attr('class',function(d) {
+					return (d.depth === 0 || d.single && d.single === true)
+						? 'source'
+						: 'destination'
+				})
+				.each(function(d,i){
+					var me = d3.select(this);
+					if (d.depth === 0 || d.single && d.single === true) {
+
+						d3.select(this.parentNode)
+							.attr("transform", function(d) {
+								d.x = d.x + 20;  // accounting for size
+								return "translate(" + d.y + "," + d.x + ")";
+							})
+						if (d.depth === 0) {
+							var offset = this.getBBox().width;
+							svg.attr("transform", "translate("+(offset+10)+",0)")
 						}
-					})
+					}
+				})
 
-					svg.each(function() {
-						var bbox = this.getBBox(),
-							container = d3.select('svg')
-								.attr('viewBox',"0 0 " + (bbox.width+20)+" "+(bbox.height))
-								.attr('preserveAspectRatio','xMinYMin')
-								// .attr('width',bbox.width)
-					})
+				svg.each(function() {
+					var bbox = this.getBBox(),
+						container = d3.select('svg')
+							.attr('viewBox',"0 0 " + (bbox.width+20)+" "+(bbox.height))
+							.attr('preserveAspectRatio','xMinYMin')
+							// .attr('width',bbox.width)
+				})
 
 
 
@@ -198,7 +229,6 @@ function load() {
 
 
 	});
-
 }
 window.onscroll = onScroll;
 window.onload = load;
